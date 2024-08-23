3634b70634dc76bbb11a225e5e3af37884616eab
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-08-23 10:36:36 +0200

    A15s: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: oppo Model: None Script version: 3.0.0

---
+++ omni_A15s.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from A15s device
+$(call inherit-product, device/oppo/A15s/device.mk)
+
+PRODUCT_DEVICE := A15s
+PRODUCT_NAME := omni_A15s
+PRODUCT_MANUFACTURER := oppo
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_oppo6765-user 10 QP1A.190711.020 bedd37e98646d3a1 release-keys"
+
+BUILD_FINGERPRINT := None
